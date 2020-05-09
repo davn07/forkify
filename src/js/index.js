4053b3b8236d85a3467e433640985d04a6dcd882
Your Name <babaloladav@gmail.com>
2020-05-09 16:40:34 +0100

    Implementing Ajax loading spinner and Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { element, elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the App
  * - Search object
@@ -23,11 +23,13 @@ const controlSearch = async () => {
         // 3) Prepare the UI for the results
         searchView.clearInput();
         searchView.clearResult();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes
          await state.search.getResults();
 
         // 5) Render results on the UI
+        clearLoader();
         searchView.renderResults(state.search.result);
         console.log(state.search.result);
     }
@@ -36,4 +38,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const gotoPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResult();
+        searchView.renderResults(state.search.result, gotoPage)
+    }
 });
